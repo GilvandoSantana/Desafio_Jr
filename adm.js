81b1c71b3f934d7f18c5ded27504b6333bc35839
GilvandoSantana <gilvando2014@gmail.com>
2025-02-22 16:15:19 -0300

    Totalmente funcional 22.02.2025

--- adm.js
+++ adm.js
@@ -1,3 +1,10 @@
-function cadastro () {
-    const nome = document.getElementById('nome')
+function administrador() {
+    const login = document.getElementById('ilogin').value
+    const senha = document.getElementById('isenha').value
+
+    if(login == "gilvando2014@gmail.com" && senha == "Gilali2024@"){
+        location.href = "dados.html"
+    }else{
+        alert('Login ou senha incorreta!')
+    }
 }
--- reclamacao.js
+++ reclamacao.js
@@ -1,10 +1,12 @@
-function cadfastro() {
-    const nome = document.getElementById('inome')
-    const cpf = document.getElementById('icpf')
-    const endereco = document.getElementById('iendereco')
-    const bairro = document.getElementById('ibairro')
-    const celular = document.getElementById('icelular')
-    const email = document.getElementById('iemail')
-    const dados = {nome, cpf, enderco, bairro, celular, email}
-    localStorage.getItem(dadosUsuario, JSON.stringify(dados))
+function cadastro() {
+    const nome = document.getElementById('inome').value
+    const cpf = document.getElementById('icpf').value
+    const endereco = document.getElementById('iendereco').value
+    const bairro = document.getElementById('ibairro').value
+    const celular = document.getElementById('icelular').value
+    const email = document.getElementById('iemail').value
+    const opcoes = document.getElementById('iturma').value
+    const reclamacao = document.getElementById('ireclamacao').value
+    const dados = {nome,cpf,endereco,bairro,celular,email,opcoes,reclamacao}
+    localStorage.setItem('dadosUsuario', JSON.stringify(dados))
 }
