ef312a470c973bd002b14dcd343e1328b2cf5bc0
GilvandoSantana <gilvando2014@gmail.com>
2025-03-07 09:50:51 -0300

    mudança de senha

--- adm.js
+++ adm.js
@@ -2,7 +2,7 @@ function administrador() {
     const login = document.getElementById('ilogin').value
     const senha = document.getElementById('isenha').value
 
-    if(login == "gilvando2014@gmail.com" && senha == "Gilali2024@"){
+    if(login == "gilvando2014@gmail.com" && senha == "12345678"){
         location.href = "dados.html"
     }else{
         alert('Login ou senha incorreta!')
